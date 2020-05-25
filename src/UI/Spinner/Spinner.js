23dd4faa1484869324137b735d1f806626678e42
Ahmed Mostafa <ahmedmoustafa771@gmail.com>
2020-05-26 00:23:52 +0200

    Testing

---
+++ src/UI/Spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Spinner.scss';
+
+
+const spinner = props => {
+    return (
+        <div class="loader">Loading...</div>
+    );
+};
+
+export default spinner;
--- src/container/MainPage.js
+++ src/container/MainPage.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MainPage.scss';
 import { Container } from 'react-bootstrap';
 
 import Header from '../components/Header/Header';
 import BinState from '../components/BinState/BinState';
+import Spinner from '../UI/Spinner/Spinner';
+
 
 const mainPage = props => {
-    return (
-        <Container fluid="true">
-            <div className="mainPage">
+    const [loading,setLoading] = useState(true);
+    let images=[];
+    const loadImages = () => {
+             images = [
+                require('../assets/2.PNG'),
+                require('../assets/24131909_377294069360392_8484384010358490932_o.jpg'),
+                require('../assets/download (1).png')
+            ];
+    };
+
+    async function asyncCall () {
+        const images2 = await loadImages();
+        setLoading(false);
+    };
+
+    asyncCall();
+
+    let mainContent = (
+        <div className="mainPage">
             <Header />
             <BinState />
         </div>
+    );
+    if (loading) {
+        mainContent = <Spinner />;
+    } else {
+        mainContent = (
+            <div className="mainPage">
+                <Header />
+                <BinState />
+            </div>);
+    }
+    return (
+        <Container fluid="true">
+            {mainContent}
         </Container>
         
     );
